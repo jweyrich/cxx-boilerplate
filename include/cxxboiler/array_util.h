29a4cd34d40b94d9e084200e84d22a402ebf7770
Jardel Weyrich <jweyrich@gmail.com>
2012-06-18 04:52:51 -0300

    Fix indentation: I prefer \t over \s{4}.

--- include/cxxboiler/array_util.h
+++ include/cxxboiler/array_util.h
@@ -7,18 +7,18 @@ namespace array {
 
 template <typename T, std::size_t N>
 size_t size(T (&array)[N]) {
-    (void)(array);
-    return N;
+	(void)(array);
+	return N;
 }
 
 template <typename T, std::size_t N>
 T* begin(T (&array)[N]) {
-    return &array[0];
+	return &array[0];
 }
 
 template <typename T, std::size_t N>
 T* end(T (&array)[N]) {
-    return &array[N];
+	return &array[N];
 }
 
 } } // namespace cxxboiler::array
--- include/cxxboiler/string_util.h
+++ include/cxxboiler/string_util.h
@@ -6,26 +6,26 @@ namespace cxxboiler {
 namespace string {
 
 std::string replace(const std::string& str, const std::string& from, const std::string& to) {
-    size_t start_pos = str.find(from);
-    if (start_pos == std::string::npos)
-        return str;
-    std::string str_copy = str;
-    str_copy.replace(start_pos, from.length(), to);
-    return str_copy;
+	size_t start_pos = str.find(from);
+	if (start_pos == std::string::npos)
+		return str;
+	std::string str_copy = str;
+	str_copy.replace(start_pos, from.length(), to);
+	return str_copy;
 }
 
 std::string replace_all(const std::string& str, const std::string& from, const std::string& to) {
-    size_t start_pos = str.find(from);
-    if (start_pos == std::string::npos)
-        return str;
-    std::string str_copy = str;
+	size_t start_pos = str.find(from);
+	if (start_pos == std::string::npos)
+		return str;
+	std::string str_copy = str;
 	const size_t from_length = from.length();
 	const size_t to_length = to.length();
 	while (start_pos != std::string::npos) {
 		str_copy.replace(start_pos, from_length, to);
 		start_pos = str_copy.find(from, start_pos + to_length);
 	}
-    return str_copy;
+	return str_copy;
 }
 
 std::string copy_range(const std::string& data, const std::string& begin, const std::string& end) {
